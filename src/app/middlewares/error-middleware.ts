c1239496c3d97b3294d45955185859085c573c45
jaovitu <jvgomes.dev@gmail.com>
2024-06-23 19:15:56 -0300

    refactor: refactoring error middleware log

--- src/app/middlewares/error-middleware.ts
+++ src/app/middlewares/error-middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { ApiError } from '../@types/errors/api-error';
+import { customLOG } from '../utils/custom-log';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function errorMiddleware(err: Error & Partial<ApiError>, req: Request, res: Response, next: NextFunction) {
@@ -7,7 +8,7 @@ function errorMiddleware(err: Error & Partial<ApiError>, req: Request, res: Resp
   const message = err.statusCode ? err.message : 'Ops! Ocorreu um erro interno no servidor!';
   const internalCode = err.statusCode ? err.internalCode : 9999;
 
-  console.log(err);
+  customLOG('ERROR HANDLER', err.message, 'error')
 
   res.status(statusCode).json({
     message,
