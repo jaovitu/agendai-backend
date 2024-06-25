ff68c20856b1efbdddff907b574bd0a62df93f32
jaovitu <jvgomes.dev@gmail.com>
2024-06-25 01:22:04 -0300

    feat: add auth middleware to specialties endpoints

--- src/app/routes/specialties-routes.ts
+++ src/app/routes/specialties-routes.ts
@@ -1,14 +1,16 @@
 import { Request, Response, Router } from 'express';
 import { createSpecialtyController } from '../use-cases/create-specialty';
 import { listSpecialtiesController } from '../use-cases/list-specialties';
+import { authMiddleware } from '../middlewares/auth-middleware';
+import { adminVerificationMiddleware } from '../middlewares/admin-verification-middleware';
 
 const specialtiesRoutes = Router();
 
-specialtiesRoutes.get('/specialties', (request: Request, response: Response) => {
+specialtiesRoutes.get('/specialties', authMiddleware, (request: Request, response: Response) => {
   return listSpecialtiesController.handler(request, response);
 });
 
-specialtiesRoutes.post('/specialties/create', (request: Request, response: Response) => {
+specialtiesRoutes.post('/specialties/create', authMiddleware, adminVerificationMiddleware, (request: Request, response: Response) => {
   return createSpecialtyController.handler(request, response);
 });
 
